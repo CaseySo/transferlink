c22a1548dccf7a61b5d6bf8b7c5a36faa29290b8
Casey J So <caseyjso@yahoo.com>
2026-04-09 13:47:06 -0700

    your message

--- js/guide.js
+++ js/guide.js
@@ -18,22 +18,64 @@ document.addEventListener("DOMContentLoaded", () => {
       title: "STUDY SPOTS",
       items: [
         { name: "Geisel Library", 
-            image: "https://via.placeholder.com/300x200", 
-            info: "Quiet and group study.", 
-            rating: 5,
+            image: "images/lib.jpg", 
+            info: "The Go To Study Spot. Group study floors 1 and 2, quiet study floors 4 to 7, and silent study floor 8. Study rooms available for reservation. Open 24/5. Audrey's Cafe open on weekdays. Events Every Week!", 
+            rating: 4.5,
             link: "https://library.ucsd.edu/"
          },
-        { name: "Revelle Study Hall", 
-            image: "https://via.placeholder.com/300x200", 
-            info: "Low distraction.", 
+        { name: "WongAvery", 
+            image: "images/wong.png", 
+            info: "Low distraction. Located near the Medical Campus. Study room reservations available.", 
             rating: 4,
-            link: "https://students.ucsd.edu/campus-services/academic-support/study-halls/revelle.html"
+            link: "https://library.ucsd.edu/about/geisel-library-and-wongavery-library/wongavery-library.html"
         },
-        { name: "Campus Cafe", 
-            image: "https://via.placeholder.com/300x200", 
-            info: "Coffee + studying.", 
+        { name: "Pinpoint Cafe", 
+            image: "images/pinpoint.jpg", 
+            info: "Two Locations, one next to the beach (outdoors), and one in the PCW (indoors). Great food, great vibes, has a UCSD discount for students", 
+            rating: 5,
+            link: "https://www.pinpoint-cafe.com/"
+         },
+         { name: "Sixth 5th floor Terrace", 
+            image: "images/sixth.jpg", 
+            info: "Great view of the ocean, many tables, but no outlets", 
             rating: 3,
-            link: "https://students.ucsd.edu/campus-services/academic-support/study-halls/revelle.html"
+            link: "https://artsandhumanities.ucsd.edu/initiatives/living-learning-neighborhood.html"
+         },
+         { name: "Computer Science and Engineering Building", 
+            image: "images/cse.jpg", 
+            info: "Quiet, no view (basement), but open at a variety of times including weekends.", 
+            rating: 3,
+            link: "https://cse.ucsd.edu/"
+         },
+         { name: "The Beach (La Jolla Shores)", 
+            image: "images/beach.jpg", 
+            info: "Take the SIO shuttle to Scripps Seaside Forum ! A bit unconventional, but a great place to study outdoors. Many cafe spots have outdoor seating. Better yet, bring a blanket. Wifi can be spotty but UCSD wifi does reach the beach. Tan, play music, and study = perfect combo!", 
+            rating: 5,
+            link: "https://transportation.ucsd.edu/campus/shuttles/sio.html"
+         },
+         { name: "Franklin Antonio Hall", 
+            image: "images/fah.jpg", 
+            info: "Pretty interior, many seating on different floors, not ideal for group study since the seating is mostly in the public eye. Has cafe on first floor open on weekdays with specials.", 
+            rating: 4,
+            link: "https://fah.ucsd.edu/"
+         },
+         { name: "Library Walk Hammocks", 
+            image: "images/hammock.jpg", 
+            info: "Campus Hammocks for relaxation and study. Located on Library walk, perfect for reading and studying. No outlets and wifi can be spotty at times.", 
+            rating: 3,
+            link: "https://library.ucsd.edu/"
+         },
+         { name: "Price Center", 
+            image: "images/price.jpg", 
+            info: "Open 24/7, food options avaiable. Not for quiet study, computers are avaiable for use, and printing is available.", 
+            rating: 4,
+            link: "https://universitycenters.ucsd.edu/visit/building-hours.html"
+         },
+         { name: "By College Study Halls", 
+            image: "images/college.png", 
+            info: "Each College has their own study hall spaces. Find one near you or most convenient!", 
+            rating: 4,
+            link: "https://students.ucsd.edu/campus-services/technology/campus-study-locations.html#roosevelt-college"
          }
       ]
     },
@@ -60,39 +102,113 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       title: "RECREATION ON CAMPUS",
       items: [
-        { name: "Library", 
-            image: "https://via.placeholder.com/300x200", 
-            info: "Books, study rooms, research help.",
-            link: "https://library.ucsd.edu/"
+        { name: "RIMAC", 
+            image: "images/rimac.jpg", 
+            info: "Largest Campus Gym, has rooms to reserve for other sports. Classes and open rec available.",
+            link: "https://recreation.ucsd.edu/facilities/"
          },
-        { name: "Student Services", 
-            image: "https://via.placeholder.com/300x200", info: "Advising and support.",
-            link: "https://students.ucsd.edu/"
+        { name: "Main Gym", 
+            image: "images/maingym.webp", 
+            info: "Smaller than RIMAC, but also less crowded",
+            link: "https://recreation.ucsd.edu/facilities/"
         },
-        { name: "Tech Support",
-            image: "https://via.placeholder.com/300x200",
-            info: "Help with WiFi and devices.",
-            link: "https://blink.ucsd.edu/techsupport/"
+        { name: "Canyonview",
+            image: "images/canyonview.webp",
+            info: "Small Gym, Climbing, and Large Pool. Times vary.",
+            link: "https://recreation.ucsd.edu/facilities/"
+        },
+        { name: "Outdoor Courts",
+            image: "images/courts.webp",
+            info: "Has open times, but some may need reservations (Pickleball + Tennis)",
+            link: "https://recreation.ucsd.edu/facilities/"
+        },
+        {
+            name: "TEC CAFE and TEC 2",
+            image: "images/tec.jpg",
+            info: "Triton Esports Center Located in the RIMAC Annex and Marshall College ",
+            link: "https://recreation.ucsd.edu/facilities/"
+        },
+        {
+            name: "Art Power + Events",
+            image: "images/artpower.jpeg",
+            info: "Performing Arts, Concerts, and MORE",
+            link: "https://artpower.ucsd.edu/"
+        },
+        {
+            name: "Craft Center",
+            image: "images/craft.jpg",
+            info: "Located in Sixth College, classes are available for multitude of crafts from painting to pottery to matcha making, as well as open studio. There are events weekly and times vary.",
+            link: "https://craftcenter.ucsd.edu/"
+        },
+        {
+            name: "Recreation Programs",
+            image: "images/recp.jpeg",
+            info: "Variety of classes available for students (surfing, yoga, etc.), as well as adventures (hiking, kayaking, etc.), gear rental is available.",
+            link: "https://recreation.ucsd.edu/"
+        },
+        {
+            name: "Intramural Sports",
+            image: "images/intramural.jpeg",
+            info: "Sports rotate every quarter. Meet new friends, play some games, and win prizes!",
+            link: "https://recreation.ucsd.edu/competitive-sports/intramurals/"
         }
+        
       ]
     },
     {
-      title: "FUN THINGS TO DO IN SAN DIEGO",
+      title: "FUN THINGS TO DO + PLACES TO VISIT IN SAN DIEGO",
       items: [
-        { name: "Library", 
-            image: "https://via.placeholder.com/300x200", 
-            info: "Books, study rooms, research help.",
-            link: "https://library.ucsd.edu/"
+        { name: "San Diego Zoo",
+            image: "https://via.placeholder.com/300x200",
+            info: "World-famous zoo with a wide variety of animals.",
+            link: "https://www.sandiegozoo.org/"
          },
-        { name: "Student Services", 
-            image: "https://via.placeholder.com/300x200", info: "Advising and support.",
+        { name: "Visit Balboa Park", 
+            image: "https://via.placeholder.com/300x200", 
+            info: "Advising and support.",
             link: "https://students.ucsd.edu/"
         },
-        { name: "Tech Support",
+        { name: "Birch Aquarium",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Explore Little Italy and Old Town",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Petco Park",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "La Jolla Cove",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Hike Torrey Pines",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Explore Convoy",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Check out the Bioluminescence",
+            image: "https://via.placeholder.com/300x200",
+            info: "Help with WiFi and devices.",
+            link: "https://blink.ucsd.edu/techsupport/"
+        },
+        { name: "Check out the Amazing Food",
             image: "https://via.placeholder.com/300x200",
             info: "Help with WiFi and devices.",
             link: "https://blink.ucsd.edu/techsupport/"
         }
+
       ]
     },
     {
@@ -180,6 +296,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       cards.appendChild(card);
+      cards.scrollLeft = 0;
+
     });
 
     // arrows
